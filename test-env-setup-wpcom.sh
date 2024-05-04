d5a6ea52fda3905b68077f121a2f3545aa99ead7
Rodel Calasagsag <od.rodel.calasagsag@gmail.com>
2024-05-04 23:58:54 +0800

    Change echo to printf

--- test-env-setup-wpcom.sh
+++ test-env-setup-wpcom.sh
@@ -7,21 +7,21 @@ wp plugin install --activate https://downloads.wordpress.org/plugin/akismet.zip 
     https://github.com/WP-API/Basic-Auth/archive/master.zip \
     https://downloads.wordpress.org/plugin/wp-mail-logging.zip
 
-echo -e "\n\n\n"
+printf "\n\n\n"
 
 echo "--------------------------------------------------------"
 echo -e "Set default theme"
 echo "--------------------------------------------------------"
 wp theme install --activate twentytwentythree
 
-echo -e "\n\n\n"
+printf "\n\n\n"
 
 echo "--------------------------------------------------------"
 echo -e "Update URL structure"
 echo "--------------------------------------------------------"
 wp rewrite structure '/%postname%/' --hard
 
-echo -e "\n\n\n"
+printf "\n\n\n"
 
 echo "--------------------------------------------------------"
 echo -e "Installing mu plugins"
@@ -31,7 +31,7 @@ wp plugin install --activate https://raw.githubusercontent.com/rodelgc/woo-e2e-s
     https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/mu-plugins/test-helper-apis.zip \
     https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/mu-plugins/wp-cache-flush.zip
 
-echo -e "\n\n\n"
+printf "\n\n\n"
 
 echo "--------------------------------------------------------"
 echo -e 'Add Customer user'
@@ -43,21 +43,21 @@ wp user create customer customer@woocommercecoree2etestsuite.com \
     --last_name='Smith' \
     --display_name='Jane Smith'
 
-echo -e "\n\n\n"
+printf "\n\n\n"
 
 echo "--------------------------------------------------------"
 echo -e 'Update Blog Name'
 echo "--------------------------------------------------------"
 wp option update blogname 'WooCommerce Core E2E Test Suite'
 
-echo -e "\n\n\n"
+printf "\n\n\n"
 
 echo "--------------------------------------------------------"
 echo -e 'Enable tracking'
 echo "--------------------------------------------------------"
 wp option update woocommerce_allow_tracking 'yes'
 
-echo -e "\n\n\n"
+printf "\n\n\n"
 
 echo "--------------------------------------------------------"
 echo -e 'Upload test images'
