8e328c2cfc20b8fa4232f31cec9cb1f7ebaf28f1
Rodel Calasagsag <od.rodel.calasagsag@gmail.com>
2024-05-04 23:55:45 +0800

    Add more changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # woo-e2e-site-setup
+
+Run this command in the site's terminal:
+```bash
+curl https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/test-env-setup-wpcom.sh | bash
+```
--- test-env-setup-wpcom.sh
+++ test-env-setup-wpcom.sh
@@ -1,23 +1,41 @@
 #!/usr/bin/env bash
 
-echo -e "Installing plugins from .wp-env.json\n"
+echo "--------------------------------------------------------"
+echo -e "Installing plugins from .wp-env.json"
+echo "--------------------------------------------------------"
 wp plugin install --activate https://downloads.wordpress.org/plugin/akismet.zip \
     https://github.com/WP-API/Basic-Auth/archive/master.zip \
     https://downloads.wordpress.org/plugin/wp-mail-logging.zip
 
-echo -e "Set default theme\n"
+echo "\n\n\n"
+
+echo "--------------------------------------------------------"
+echo -e "Set default theme"
+echo "--------------------------------------------------------"
 wp theme install --activate twentytwentythree
 
-echo -e "Update URL structure\n"
+echo "\n\n\n"
+
+echo "--------------------------------------------------------"
+echo -e "Update URL structure"
+echo "--------------------------------------------------------"
 wp rewrite structure '/%postname%/' --hard
 
-echo -e "Installing mu plugins\n"
-wp plugin install --activate https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/mu-plugins/filter-setter.php \
-    https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/mu-plugins/process-waiting-actions.php \
-    https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/mu-plugins/test-helper-apis.php \
-    https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/mu-plugins/wp-cache-flush.php
+echo "\n\n\n"
 
-echo -e 'Add Customer user \n'
+echo "--------------------------------------------------------"
+echo -e "Installing mu plugins"
+echo "--------------------------------------------------------"
+wp plugin install --activate https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/mu-plugins/filter-setter.zip \
+    https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/mu-plugins/process-waiting-actions.zip \
+    https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/mu-plugins/test-helper-apis.zip \
+    https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/mu-plugins/wp-cache-flush.zip
+
+echo "\n\n\n"
+
+echo "--------------------------------------------------------"
+echo -e 'Add Customer user'
+echo "--------------------------------------------------------"
 wp user create customer customer@woocommercecoree2etestsuite.com \
     --user_pass=password \
     --role=customer \
@@ -25,13 +43,25 @@ wp user create customer customer@woocommercecoree2etestsuite.com \
     --last_name='Smith' \
     --display_name='Jane Smith'
 
-echo -e 'Update Blog Name \n'
+echo "\n\n\n"
+
+echo "--------------------------------------------------------"
+echo -e 'Update Blog Name'
+echo "--------------------------------------------------------"
 wp option update blogname 'WooCommerce Core E2E Test Suite'
 
-echo -e 'Enable tracking\n'
+echo "\n\n\n"
+
+echo "--------------------------------------------------------"
+echo -e 'Enable tracking'
+echo "--------------------------------------------------------"
 wp option update woocommerce_allow_tracking 'yes'
 
-echo -e 'Upload test images \n'
+echo "\n\n\n"
+
+echo "--------------------------------------------------------"
+echo -e 'Upload test images'
+echo "--------------------------------------------------------"
 wp media import https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/images/image-01.png \
     https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/images/image-02.png \
     https://raw.githubusercontent.com/rodelgc/woo-e2e-site-setup/trunk/images/image-03.png
